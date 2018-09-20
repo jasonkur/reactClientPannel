15d042ab6fa12c253fc913b0e08c4768a8b84c6e
jasonww11 <jasonww11@hotmail.com>
2018-09-19 20:16:01 -0400

    adding router to the react allowing multiple pages

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default () => {
+  return (
+    <div>
+      <h1 className="display-4"> About Page</h1>
+      <p className="lead">
+        Simple app for learning React and manage contacts info
+      </p>
+    </div>
+  );
+};
